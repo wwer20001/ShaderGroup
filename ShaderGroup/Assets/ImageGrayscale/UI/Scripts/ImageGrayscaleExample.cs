780a161a93f8fc1ef4a3acc2627e166d97338e6d
wwer20001 <wwer20001@naver.com>
2019-01-30 10:00:22 +0900

    add Image Grayscale shader example.
    add Image Grayscale shader example.

---
+++ ShaderGroup/Assets/ImageGrayscale/UI/Scripts/ImageGrayscaleExample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageGrayscaleExample : MonoBehaviour
+{
+    public Material grayscaleMaterial;
+    private Image image;
+
+    void Start()
+    {
+        Material m = new Material(grayscaleMaterial);
+
+        image = GetComponent<Image>();
+        image.material = m;
+    }
+
+    float timer = 0f;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        image.material.SetFloat("_Saturation", (Mathf.Sin(timer) + 1f) * 0.5f);
+    }
+}
